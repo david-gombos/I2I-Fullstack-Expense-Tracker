4225991ad54ae597aadc46ad186a1be13e3827dc
david-gombos <132453001+david-gombos@users.noreply.github.com>
2024-11-26 13:53:26 +0100

    feat: commit for HCK-34

--- backend/src/main/java/com/fullStack/expenseTracker/repository/CategoryRepository.java
+++ backend/src/main/java/com/fullStack/expenseTracker/repository/CategoryRepository.java
@@ -1,13 +1,12 @@
 package com.fullStack.expenseTracker.repository;
 
 import com.fullStack.expenseTracker.models.Category;
-import com.fullStack.expenseTracker.models.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-    boolean existsByCategoryNameAndTransactionType(String categoryName, TransactionType transactionType);
-
+    Optional<Category> findByName(String name);
 }
