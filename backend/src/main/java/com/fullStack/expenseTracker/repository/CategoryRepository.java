2c5bb2bab80ecb55f89092dfcf52e21699ff78b7
david-gombos <132453001+david-gombos@users.noreply.github.com>
2024-11-26 13:49:30 +0100

    feat: commit for HCK-34

--- backend/src/main/java/com/fullStack/expenseTracker/repository/CategoryRepository.java
+++ backend/src/main/java/com/fullStack/expenseTracker/repository/CategoryRepository.java
@@ -1,32 +1,12 @@
 package com.fullStack.expenseTracker.repository;
 
-import com.fullStack.expenseTracker.entities.Category;
+import com.fullStack.expenseTracker.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    
-    /**
-     * Checks if a category exists by name and transaction type.
-     * 
-     * @param categoryName the name of the category.
-     * @param transactionType the transaction type of the category.
-     * @return true if the category exists, false otherwise.
-     */
-    boolean existsByCategoryNameAndTransactionType(String categoryName, TransactionType transactionType);
-
-    /**
-     * Save a new category entity in the database.
-     *
-     * @param category the category entity to save.
-     * @return the saved category entity.
-     */
-    default Category saveCategory(Category category) {
-        try {
-            return save(category);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to save category: " + e.getMessage(), e);
-        }
-    }
+    Optional<Category> findByName(String name);
 }
