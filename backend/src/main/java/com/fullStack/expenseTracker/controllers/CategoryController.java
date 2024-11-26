b3d58f488ffbe93d38d6b118b71e7a9c3bfa2839
david-gombos <132453001+david-gombos@users.noreply.github.com>
2024-11-26 13:49:25 +0100

    feat: commit for HCK-34

--- backend/src/main/java/com/fullStack/expenseTracker/controllers/CategoryController.java
+++ backend/src/main/java/com/fullStack/expenseTracker/controllers/CategoryController.java
@@ -2,43 +2,49 @@ package com.fullStack.expenseTracker.controllers;
 
 import com.fullStack.expenseTracker.dto.ApiResponseDto;
 import com.fullStack.expenseTracker.dto.ApiResponseStatus;
-import com.fullStack.expenseTracker.exceptions.CategoryServiceLogicException;
+import com.fullStack.expenseTracker.models.Category;
 import com.fullStack.expenseTracker.services.CategoryService;
+import com.fullStack.expenseTracker.services.exceptions.CategoryServiceLogicException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/mywallet/category")
-@CrossOrigin(origins = "*")
-@Slf4j
-@Validated
 public class CategoryController {
 
-    private final CategoryService categoryService;
-
     @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    private CategoryService categoryService;
 
     @PostMapping("/add")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<ApiResponseDto<?>> addCategory(@RequestBody @Valid CategoryRequestDto categoryRequestDto) {
+    public ResponseEntity<ApiResponseDto<?>> addCategory(@Validated @RequestBody Category category) {
         try {
-            return categoryService.addCategory(categoryRequestDto);
+            Category newCategory = categoryService.addCategory(category);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponseDto<>(ApiResponseStatus.SUCCESS, HttpStatus.CREATED, newCategory));
         } catch (CategoryServiceLogicException e) {
-            log.error("Failed to add new category: " + e.getMessage(), e);
+            log.error("Failed to add category: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ApiResponseDto<>(ApiResponseStatus.FAILED, HttpStatus.BAD_REQUEST, "Failed to add new category: " + e.getMessage()));
-        } catch (Exception e) {
-            log.error("Unexpected error occurred while adding new category: " + e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ApiResponseDto<>(ApiResponseStatus.FAILED, HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred. Please try again later."));
+                    .body(new ApiResponseDto<>(ApiResponseStatus.FAILED, HttpStatus.BAD_REQUEST, e.getMessage()));
         }
+    }
+
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ApiResponseDto<?>> disableOrEnableCategory(@RequestParam("categoryId") int categoryId) 
+            throws CategoryServiceLogicException, CategoryNotFoundException {
+        return categoryService.enableOrDisableCategory(categoryId);
+    }
+
+    @GetMapping("/getAll")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ApiResponseDto<?>> getAllCategories() {
+        return categoryService.getCategories();
     }
 }
